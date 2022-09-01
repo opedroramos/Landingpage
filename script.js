ce8ff90fcb35cfffcfad39b685a5010e53ab2d7d
Pedro Ramos <opedroramosbr@gmail.com>
2022-08-31 21:48:19 -0300

    opa

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ setTimeout(
         var elemento = document.querySelector('#botaopai')
         var elemento11 = document.querySelector('#pagamentos')
         var elemento13 = document.querySelector('#vagas')
+        var elemento18 = document.querySelector('#provapai')
+
 
         elemento.style.display = "flex";
         elemento11.style.display = "flex";
         elemento13.style.display = "flex";
+        elemento18.style.display = "flex";
+
 
     }, /*tempo em milisegundos*/ 500)
