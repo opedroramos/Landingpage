2cd166bf21355a589f521456d38ee74bb0b01e71
Pedro Ramos <opedroramosbr@gmail.com>
2022-11-16 15:40:39 -0300

    00

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ setTimeout(
         var elemento13 = document.querySelector('#vagas')
         var elemento20 = document.querySelector('#objeções')
         var elemento21 = document.querySelector('#preço')
+        var elemento22 = document.querySelector('#addcarrinho')
         //var elemento18 = document.querySelector('#provapai')
 
 
@@ -13,6 +14,7 @@ setTimeout(
         elemento13.style.display = "flex";
         elemento20.style.display = "flex"
         elemento21.style.display = "flex"
+        elemento22.style.display = "block"
         //elemento18.style.display = "flex";
 
 
